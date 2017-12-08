273493521d855573b32d624a309176ee581fa6c1
Shalin Shah <shalin.d.shah@colorado.edu>
2017-12-08 12:54:21 -0700

    Initial project commit

---
+++ src/main/java/com/foodzie/foodzie/FoodzieApplication.java
@@ -0,0 +1,12 @@
+package com.foodzie.foodzie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodzieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodzieApplication.class, args);
+	}
+}
