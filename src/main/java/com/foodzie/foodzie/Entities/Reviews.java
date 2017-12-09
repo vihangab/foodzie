cca24300ecb9f5184260c472e1c661e0c753ba2e
Shalin Shah <shalin.d.shah@colorado.edu>
2017-12-09 14:25:09 -0700

    Created Review class

--- src/main/java/com/foodzie/foodzie/Entities/Reviews.java
+++ src/main/java/com/foodzie/foodzie/Entities/Reviews.java
@@ -1,4 +1,61 @@
 package com.foodzie.foodzie.Entities;
 
 public class Reviews {
+
+    private Long Id;
+    private Double rating;
+    private String comment;
+    private Outlets outlet;
+    private Person reviewer;
+
+    public Reviews() {
+    }
+
+    public Reviews(Long id, Double rating, String comment, Outlets outlet, Person reviewer) {
+        Id = id;
+        this.rating = rating;
+        this.comment = comment;
+        this.outlet = outlet;
+        this.reviewer = reviewer;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Outlets getOutlet() {
+        return outlet;
+    }
+
+    public void setOutlet(Outlets outlet) {
+        this.outlet = outlet;
+    }
+
+    public Person getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(Person reviewer) {
+        this.reviewer = reviewer;
+    }
 }
