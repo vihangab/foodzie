50d8b0aa1df93190b81915536e216a44a63d1c6d
Shalin Shah <shalin.d.shah@colorado.edu>
2017-12-10 14:13:56 -0700

    Removed Welcome Controller and added Person Controller

---
+++ src/main/java/com/foodzie/foodzie/Controller/PersonController.java
@@ -0,0 +1,43 @@
+package com.foodzie.foodzie.Controller;
+
+import com.foodzie.foodzie.DAO.PersonDAO;
+import com.foodzie.foodzie.Entities.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PersonController {
+
+    @Autowired
+    private PersonDAO personDAO;
+
+    @RequestMapping("/welcome")
+    public String Welcome() {
+        return "Welcome to Foodzie";
+    }
+
+    @RequestMapping("/persons")
+    public List<Person> getPersons(){
+        List<Person> persons = new ArrayList<>();
+        personDAO.findAll()
+                .forEach(persons::add);
+        return persons;
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "/person/{stringId}")
+    public Person getPerson(@PathVariable String stringId){
+        int Id = Integer.valueOf(stringId);
+        return personDAO.findOne(Id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/persons")
+    public void addPerson(@RequestBody Person person){
+        personDAO.saveAndFlush(person);
+    }
+
+
+
+}
