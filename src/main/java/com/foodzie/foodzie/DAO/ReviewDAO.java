b971e335938a584f40b126d139663ad42af27697
JeetBaru <jeet.baru@colorado.edu>
2017-12-10 14:02:33 -0700

    Updated Review DAO

--- src/main/java/com/foodzie/foodzie/DAO/ReviewDAO.java
+++ src/main/java/com/foodzie/foodzie/DAO/ReviewDAO.java
@@ -1,7 +1,19 @@
 package com.foodzie.foodzie.DAO;
 
+import com.foodzie.foodzie.Entities.Outlet;
+import com.foodzie.foodzie.Entities.Person;
 import com.foodzie.foodzie.Entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ReviewDAO extends JpaRepository<Review, Long>{
+import java.util.List;
+
+@Repository
+public interface ReviewDAO extends JpaRepository<Review, Long> {
+
+    public List<Review> findByOutlet(@Param("outlet") Outlet outlet);
+
+    public List<Review> findByPerson(@Param("person")Person person);
+
 }
