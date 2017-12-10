b4992ce2b3bebf77d37121832ff6ebd3962e6aac
Shalin Shah <shalin.d.shah@colorado.edu>
2017-12-10 14:00:40 -0700

    Updated Person DAO

--- src/main/java/com/foodzie/foodzie/DAO/PersonDAO.java
+++ src/main/java/com/foodzie/foodzie/DAO/PersonDAO.java
@@ -1,9 +1,22 @@
 package com.foodzie.foodzie.DAO;
 
 import com.foodzie.foodzie.Entities.Person;
+import com.foodzie.foodzie.Entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PersonDAO extends JpaRepository<Person,Integer>{
+
+    public List<Person> findByPasswordIs(@Param("password") String password);
+
+    public List<Person> findByNameIs(@Param("name") String name);
+
+    public Person findByUsernameIs(@Param("username") String username);
+
+    public Person findByIdIs(@Param("Id") Integer Id);
+
 }
