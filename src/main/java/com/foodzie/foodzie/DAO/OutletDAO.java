4bf8e716033c81229691c5f08969c05d4484092c
vihangab <bare.vihanga5@gmail.com>
2017-12-10 13:59:20 -0700

    Updated OutletDAO

--- src/main/java/com/foodzie/foodzie/DAO/OutletDAO.java
+++ src/main/java/com/foodzie/foodzie/DAO/OutletDAO.java
@@ -1,7 +1,15 @@
 package com.foodzie.foodzie.DAO;
 
 import com.foodzie.foodzie.Entities.Outlet;
+import com.foodzie.foodzie.Entities.Person;
+import com.foodzie.foodzie.Entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OutletDAO extends JpaRepository<Outlet, Long> {
+
+    public Outlet findById(@Param("Id") Long Id);
+
 }
