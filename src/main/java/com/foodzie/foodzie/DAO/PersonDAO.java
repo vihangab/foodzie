7df8db3817ab5bd600dbdd5224620ce98cbd6f8d
JeetBaru <jeet.baru@colorado.edu>
2017-12-09 20:43:52 -0700

    Changes made to person dao

--- src/main/java/com/foodzie/foodzie/DAO/PersonDAO.java
+++ src/main/java/com/foodzie/foodzie/DAO/PersonDAO.java
@@ -2,6 +2,8 @@ package com.foodzie.foodzie.DAO;
 
 import com.foodzie.foodzie.Entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PersonDAO extends JpaRepository<Person,Long>{
+@Repository
+public interface PersonDAO extends JpaRepository<Person,Integer>{
 }
