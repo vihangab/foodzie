32b691c44dc5b8c21daca925c59e034244395752
JeetBaru <jeet.baru@colorado.edu>
2017-12-11 11:41:01 -0700

    Made changes to login controller

--- src/main/java/com/foodzie/foodzie/Controller/LoginController.java
+++ src/main/java/com/foodzie/foodzie/Controller/LoginController.java
@@ -3,32 +3,47 @@ package com.foodzie.foodzie.Controller;
 import com.foodzie.foodzie.DAO.PersonDAO;
 import com.foodzie.foodzie.Entities.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
 
     @Autowired
     PersonDAO personDAO;
 
+    @RequestMapping(method = RequestMethod.GET, value = "/signup")
+    public String showSignup(Model model) {
+        model.addAttribute("Person", new Person());
+        return "signup-page";
+    }
+
     @RequestMapping(method = RequestMethod.POST, value = "/signup")
-    public void addPerson(@RequestBody Person person) {
+    public String addPerson(Model model, @ModelAttribute Person person) {
         personDAO.saveAndFlush(person);
+        return  "redirect:/login-page";
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/login")
+    public String showLogin(Model model) {
+        model.addAttribute("Person", new Person());
+        return "login-page";
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/login")
-    public String loginPerson(@RequestBody Person person){
+    public String loginPerson(Model model, @ModelAttribute Person person) {
         Person loginPerson = personDAO.findByUsernameIs(person.getUsername());
-        if (loginPerson.getUsername().isEmpty()){
+        if (loginPerson.getUsername().isEmpty()) {
             return "Failed";
+        } else if (loginPerson.getPassword().equals(person.getPassword())) {
+            return "Success" + "Account Type : " + String.valueOf(loginPerson.getAccess());
         }
-        else if(loginPerson.getPassword().equals(person.getPassword())){
-            return "Success" + "Account Type : "  + String.valueOf(loginPerson.getAccess());
-        }
-        return "Failed - ";
+        return "Failed";
     }
 
 }
